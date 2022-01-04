164bfe82fe3bfd8d52b4319707dc24b0616e0c31
Max <84286104+Augustine2630@users.noreply.github.com>
2022-01-04 20:56:14 +0300

    Add files via upload

---
+++ server/ClientHandler.java
@@ -0,0 +1,80 @@
+package server;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ClientHandler implements Runnable {
+		
+		
+    private Server server;
+		
+    private PrintWriter outMessage;
+		
+    private Scanner inMessage;
+    private static final String HOST = "172.20.10.14";
+    private static final int PORT = 3443;   
+		
+    private Socket clientSocket = null;
+	
+    private static int clients_count = 0;
+
+		// take client socket
+    public ClientHandler(Socket socket, Server server) {
+        try {
+            clients_count++;
+            this.server = server;
+            this.clientSocket = socket;
+            this.outMessage = new PrintWriter(socket.getOutputStream());
+            this.inMessage = new Scanner(socket.getInputStream());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+		// ovveride run() when call Thread(client).start();
+    @Override
+    public void run() {
+        try {
+            while (true) {
+								//server send the message
+                server.sendMessageToAllClients("Новый участник вошёл в чат");
+               // server.sendMessageToAllClients("Клиентов в чате = " + clients_count);
+                break;
+            }
+
+            while (true) {
+                
+                if (inMessage.hasNext()) {
+                    String clientMessage = inMessage.nextLine();							
+                    if (clientMessage.equalsIgnoreCase("##session##end##")) {
+                        break;
+                    }									
+                    System.out.println(clientMessage);						
+                    server.sendMessageToAllClients(clientMessage);
+                }
+                Thread.sleep(100);
+            }
+        }
+        catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        finally {
+            this.close();
+        }
+    }
+    public void sendMsg(String msg) {
+        try {
+            outMessage.println(msg);
+            outMessage.flush();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void close() {
+        server.removeClient(this);
+        clients_count--;
+       // server.sendMessageToAllClients("Клиентов в чате = " + clients_count);
+    }
+}
